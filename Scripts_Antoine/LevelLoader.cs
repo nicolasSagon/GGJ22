99d4bc04bdc789cc0e0dfa048fd57d38d8614d59
nicolas sagon <nicolas.sagon.p@thalesdigital.io>
2022-01-28 19:25:34 +0100

    Add Antoine scripts

---
+++ Scripts_Antoine/LevelLoader.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Button bouttonMenu;
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public void LoadSceneMenuFonction()
+    {
+        StartCoroutine(LoadSceneMenu());
+    }
+
+    IEnumerator LoadSceneMenu()
+    {
+        //if (LevelIndex >= 2) yield break;
+        //play animation
+        transition.SetTrigger("Start");
+
+        //wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //load scene
+        SceneManager.LoadScene("SceneMenu");
+    }
+
+    public void LoadScenePrincipaleFonction()
+        {
+           StartCoroutine(LoadScenePrincipale());
+        }
+
+    IEnumerator LoadScenePrincipale()
+    {
+        //if (LevelIndex >= 2) yield break;
+        //play animation
+        transition.SetTrigger("Start");
+
+        //wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //load scene
+        SceneManager.LoadScene("ScenePrincipale");
+    }
+
+    public void LoadSceneCreditsFonction()
+    {
+        StartCoroutine(LoadSceneCredits());
+    }
+
+    IEnumerator LoadSceneCredits()
+    {
+        //if (LevelIndex >= 2) yield break;
+        //play animation
+        transition.SetTrigger("Start");
+
+        //wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //load scene
+        SceneManager.LoadScene("SceneCredits");
+    }
+
+    public void QuitGame()
+    {
+        print("quit game!");
+        Application.Quit();
+    }
+}
---
+++ Scripts_Antoine/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPause = false;
+    //public Button Button;
+
+    void Update()
+    {
+        //Button.onClick.AddListener(ButtonClick);
+    }
+
+    //void ButtonClick()
+    //{
+    //   if (GameIsPause)
+    //   {
+    //        Resume();
+    //   }
+    //   else
+    //   {
+    //       Pause();
+    //   }
+    //}
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    public void LoadMenu ()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SceneMenu");
+    }
+
+    public void Quit()
+    {
+        print("quit");
+        Application.Quit();
+    }
+}
