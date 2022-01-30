d29a640513a04c3fe8115cba5209d3edfd150e9f
Zbraah <pito69260@hotmail.fr>
2022-01-30 17:07:17 +0100

    Update ScoreDisplayer.cs

--- ScoreDisplayer.cs
+++ ScoreDisplayer.cs
@@ -7,37 +7,40 @@ public class ScoreDisplayer : MonoBehaviour
 {
     public Sprite p1score0, p1score1, p1score2, p1score3, p1score4, p2score1, p2score2, p2score3, p2score4, p2score0;
     public GameObject p1lifebar, p2lifebar;
-    private static Image p1sprite, p2sprite;
+    private Image p1sprite, p2sprite;
     private Player playerOne, playerTwo;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        playerOne = GameObject.FindWithTag("P1").GetComponent<Player>();
-        playerTwo = GameObject.FindWithTag("P2").GetComponent<Player>();
-
-        p1sprite = p1lifebar.AddComponent<Image>();
+        p1sprite = p1lifebar.GetComponent<Image>();
         p1sprite.sprite = p1score0;
-        p2sprite = p2lifebar.AddComponent<Image>();
+        p2sprite = p2lifebar.GetComponent<Image>();
         p2sprite.sprite = p2score0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerOne == null){
+            playerOne = GameObject.FindWithTag("P1").GetComponent<Player>();
+        }
+        if (playerTwo == null){
+            playerTwo = GameObject.FindWithTag("P2").GetComponent<Player>();
+        }
         switch (playerOne.GetScore()){
             case 0:
                 p1sprite.sprite = p1score0;
                 break;
-            case 1:
+            case -1:
                 p1sprite.sprite = p1score1;
                 break;
-            case 2:
+            case -2:
                 p1sprite.sprite = p1score2;
                 break;
-            case 3:
+            case -3:
                 p1sprite.sprite = p1score3;
                 break;
-            case 4:
+            case -4:
                 p1sprite.sprite = p1score4;
                 break;
             default:
@@ -47,16 +50,16 @@ public class ScoreDisplayer : MonoBehaviour
             case 0:
                 p2sprite.sprite = p2score0;
                 break;
-            case 1:
+            case -1:
                 p2sprite.sprite = p2score1;
                 break;
-            case 2:
+            case -2:
                 p2sprite.sprite = p2score2;
                 break;
-            case 3:
+            case -3:
                 p2sprite.sprite = p2score3;
                 break;
-            case 4:
+            case -4:
                 p2sprite.sprite = p2score4;
                 break;
             default:
